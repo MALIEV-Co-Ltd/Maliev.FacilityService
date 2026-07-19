d6b6f7ceb36e5ee670c63eb3cc9f86bb44cf091b
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-07-19 18:05:15 +0700

    style: normalize Facility test formatting

--- Maliev.FacilityService.Tests/PostgresFixture.cs
+++ Maliev.FacilityService.Tests/PostgresFixture.cs
@@ -15,7 +15,7 @@ public class PostgresCollection : ICollectionFixture<PostgresFixture>
 
 public class PostgresFixture : IAsyncLifetime
 {
-    private readonly PostgreSqlContainer _dbContainer = 
+    private readonly PostgreSqlContainer _dbContainer =
 #pragma warning disable CS0618
         new PostgreSqlBuilder()
         .WithImage("postgres:15-alpine")
